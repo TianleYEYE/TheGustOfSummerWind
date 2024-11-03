1ad90b934f940ca8072e37269a3dae0bccb8373b
TianleYEYE <3484768630@qq.com>
2024-11-03 20:38:25 +0800

    上一版有点问题，重新弄了一下

--- Source/TheGustOfSummerWind/Public/UI/QuitGameUI/SW_QuitGameUI.h
+++ Source/TheGustOfSummerWind/Public/UI/QuitGameUI/SW_QuitGameUI.h
@@ -3,8 +3,6 @@
 #include "CoreMinimal.h"
 #include "Game/SW_ScriptManager.h"
 #include "UI/SW_UIBase.h"
-#include "UI/Button/BTN_ButtonInQuitGame/BTN_No.h"
-#include "UI/Button/BTN_ButtonInQuitGame/BTN_Yes.h"
 #include "SW_QuitGameUI.generated.h"
 
 
@@ -14,11 +12,6 @@ class THEGUSTOFSUMMERWIND_API USW_QuitGameUI : public USW_UIBase
 {
 	GENERATED_BODY()
 public:
-	UPROPERTY(EditAnywhere,BlueprintReadWrite,meta=(BindWidget))
-	TObjectPtr<UBTN_Yes>BP_Yes;
-
-	UPROPERTY(EditAnywhere,BlueprintReadWrite,meta=(BindWidget))
-	TObjectPtr<UBTN_No>BP_No;
 
 	UPROPERTY(Transient,EditAnywhere,BlueprintReadWrite,meta=(BindWidgetAnim))
 	TObjectPtr<UWidgetAnimation>Fade;
@@ -30,13 +23,4 @@ public:
 	
 	virtual void NativeConstruct() override;
 
-	UFUNCTION()
-	void QuitGame();
-
-	UFUNCTION()
-	void ReturnGame();
-
-	void QuitGameImplement();
-
-	void ReturnGameImlement();
 };
